bcd1e34ccbb25b425aa77fd8ff1a59626cde6269
Benedicta-Asare <lilbengilla@gmail.com>
2023-09-30 22:28:02 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,19 +1,11 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP, text
+from sqlalchemy import Column, Integer, String, DateTime, func
 from database import Base
 
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    content = Column(String)
-    created_at = Column(
-        TIMESTAMP(timezone=True), 
-        server_default=text("now()"), 
-        nullable=False
-    )
-    updated_at = Column(
-        TIMESTAMP(timezone=True), 
-        server_default=text("now()"), 
-        onupdate=text("now()"), 
-        nullable=False
-    )
+    #author = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
